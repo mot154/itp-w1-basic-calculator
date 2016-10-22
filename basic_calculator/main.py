f944af40f4e21103b02b0f29141fabd793880490
mot154 <mutasem.mba@gmail.com>
2016-10-22 00:38:26 +0000

    First project solution

--- basic_calculator/main.py
+++ basic_calculator/main.py
@@ -2,4 +2,17 @@
 
 
 def basic_calculator(num1, num2, operation):
-    pass
+    if operation == 'add':
+        return num1 + num2
+    if operation == 'subtract':
+        return num1 - num2
+    if operation == 'multiply':
+        return num1 * num2
+    if operation == 'divide':
+        return num1 / num2
+    else:
+        return 'Invalid operation'
+        
+
+        
+
